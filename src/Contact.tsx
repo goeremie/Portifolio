f2c15132ac5e4fd047415eb616c999264b128f8c
goeremie <gabriel.eremie@yahoo.com>
2025-08-07 18:00:17 -0700

    link changes

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -40,38 +40,41 @@ const Contact: React.FC = () => {
           >
             Email
           </a>
-          {/*
-          <a
-            href="https://github.com/goeremie"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-4 rounded-xl shadow-md bg-white hover:bg-gray-100 transition flex justify-center"
-            style={{
-              color: "#1F2937", // gray-800
-              fontWeight: "bold",
-              fontSize: "1.5rem",
-              textDecoration: "none",
-              textAlign: "center",
-            }}
+          {/* */}
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-md mt-12 px-4"
+            style={{ marginTop: "50px" }}
           >
-            GitHub
-          </a>
+            <a
+              href="https://github.com/goeremie"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-4 rounded-xl shadow-md bg-white hover:bg-gray-100 transition flex justify-center"
+              style={{
+                color: "#1F2937", // gray-800
+                fontWeight: "bold",
+                fontSize: "1.5rem",
+                textDecoration: "none",
+                textAlign: "center",
+              }}
+            >
+              GitHub
+            </a>
 
-        
-
-          <a
-            href="tel:+17785124454"
-            className="p-4 rounded-xl shadow-md bg-white hover:bg-green-50 transition flex justify-center"
-            style={{
-              color: "#047857", // green-700
-              fontWeight: "bold",
-              fontSize: "1.5rem",
-              textDecoration: "none",
-              textAlign: "center",
-            }}
-          >
-            Phone
-          </a> */}
+            <a
+              href="tel:+17785124454"
+              className="p-4 rounded-xl shadow-md bg-white hover:bg-green-50 transition flex justify-center"
+              style={{
+                color: "#047857", // green-700
+                fontWeight: "bold",
+                fontSize: "1.5rem",
+                textDecoration: "none",
+                textAlign: "center",
+              }}
+            >
+              Phone
+            </a>
+          </div>
         </div>
       </div>
     </div>
